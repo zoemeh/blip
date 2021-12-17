d2013b9438a76e5009b467516f274003506e245f
Eduardo Cruz-Betances <eduardomcbt@gmail.com>
2021-12-16 21:16:47 -0400

    blips and users

--- app/models/blip.rb
+++ app/models/blip.rb
@@ -1,3 +1,4 @@
 class Blip < ApplicationRecord
   belongs_to :user
+  validates :content, length: { within: 1..140 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,15 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :realname, presence: true
+  validates :username, uniqueness: true
+  
+  has_many :blips
+
+  def mentions
+    # FIXME: this matches blips where the mentions is inside words
+    # ex: 'a blip@myusermention' where 'myuser' is a username; this is wrong.
+    Blip.where("content LIKE ?", "%@#{username}%")
+  end
 end
