80a2269b5da9fd85d21aac0ee0340c19e0a7fb30
Eduardo Cruz-Betances <eduardomcbt@gmail.com>
2021-12-16 21:13:14 -0400

    BlipFormComponent

---
+++ test/components/timeline/blip_form_component_test.rb
@@ -0,0 +1,10 @@
+require "test_helper"
+
+class Timeline::BlipFormComponentTest < ViewComponent::TestCase
+  def test_component_renders_something_useful
+    # assert_equal(
+    #   %(<span>Hello, components!</span>),
+    #   render_inline(Timeline::BlipFormComponent.new(message: "Hello, components!")).css("span").to_html
+    # )
+  end
+end
